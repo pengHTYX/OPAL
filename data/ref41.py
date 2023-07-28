b1ac854d5080c336603135b3987b326abaeab288
Joey1998hub <50704613+Joey1998hub@users.noreply.github.com>
2023-07-28 18:09:17 +0800

    Add files via upload

--- data/ref41.py
+++ data/ref41.py
@@ -2,13 +2,23 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import copy
+from posix import listdir
 
-import os
+from torch.utils.data.dataset import T
+from torchvision import transforms
+
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+
 import os
 import imageio
+from tqdm import tqdm
+
+
+dtu_root = './dataset/ref41/'
+mytransforms = transforms.Compose([transforms.ToTensor()])
 
 def load_ref41(dir_LFimages):# 541*376
     traindata_all=np.zeros((len(dir_LFimages), 368, 512, 9, 9, 3),np.uint8)
@@ -18,9 +28,9 @@ def load_ref41(dir_LFimages):# 541*376
         print(dir_LFimage)
         for i in range(81):
             try:
-                tmp  = np.float32(imageio.imread(opt.root+dir_LFimage+'/%d.png' % (i+1)))[4:372,14:526,:] # load LF images(9x9) 
+                tmp  = np.float32(imageio.imread(dtu_root+dir_LFimage+'/%d.png' % (i+1)))[4:372,14:526,:] # load LF images(9x9) 
             except:
-                print(opt.root+dir_LFimage+'/%d.png' % i)
+                print(dtu_root+dir_LFimage+'/%d.png' % i)
             traindata_all[image_id,:,:,i//9,i-9*(i//9),:]=tmp  
             del tmp
         image_id=image_id+1
@@ -37,11 +47,16 @@ class TrainDataset(Dataset):
         self.use_v = opt.use_views
         self.inc = opt.input_c
         print('Load training data...')
-        dir_LFimages = os.listdir(opt.root+'train_array/')
+        dir_LFimages = listdir(dtu_root+'train_array/')
         self.traindata_all, self.traindata_label = load_ref41(['train_array/'+sub+'/' for sub in dir_LFimages])
+        
+        # traindata_90d,traindata_0d,traindata_45d,traindata_m45d,_ = generate_traindata512(traindata_all,traindata_label, Setting02_AngualrViews) 
+        print('Load training data... Complete')  
+
+      
 
     def __len__(self):
-        return 8000
+        return 4000
 
     def __getitem__(self, index):
         index = index // 80
@@ -89,17 +104,19 @@ class TrainDataset(Dataset):
         
 
 class TestDataset(Dataset):
-    def __init__(self, opt):
+    def __init__(self, opt, is_lytro=False, transform=None):
         self.opt = opt
         self.Setting02_AngualrViews = np.array([0,1,2,3,4,5,6,7,8])
    
         print('Load test data...') 
-        self.dir_LFimages = os.listdir(opt.root)
-        # self.dir_LFimages = ['IMG_1328_eslf']
+        # self.dir_LFimages = listdir(dtu_root)[:5]
+        self.dir_LFimages = ['test_array/IMG_1411_eslf/', 'test_array/IMG_1541_eslf/', 'test_array/IMG_1328_eslf/', 'test_array/IMG_1554_eslf/', 'test_array/IMG_1586_eslf/']
 
         self.valdata_all, self.valdata_label = load_ref41(self.dir_LFimages)
         if opt.input_c == 1 :
             self.valdata_all = np.expand_dims(self.valdata_all[:,:,:,:,:,0] * 0.299 + self.valdata_all[:,:,:,:,:,1] * 0.587 + self.valdata_all[:,:,:,:,:,2] * 0.114, -1)
+        print('Load test data... Complete') 
+
 
     def __len__(self):
         return len(self.dir_LFimages)
@@ -114,6 +131,10 @@ class TestDataset(Dataset):
         valid_data = self.valdata_all / 255.
         test_label = self.valdata_label / 255.
         name = self.dir_LFimages[index]
+        # test_x = [torch.FloatTensor(np.transpose(valid_data[index, :,:, 4, j,:], (2,0,1))) for j in range(4-center, 5+center)]
+        # test_y = [torch.FloatTensor(np.transpose(valid_data[index, :,:, j, 4,:], (2,0,1))) for j in range(4-center, 5+center)]
+        # test_45 = [torch.FloatTensor(np.transpose(valid_data[index, :,:, j, j,:], (2,0,1))) for j in range(4-center, 5+center)]
+        # test_135 = [torch.FloatTensor(np.transpose(valid_data[index, :,:, j, 8-j,:], (2,0,1))) for j in range(4-center, 5+center)]
         valid_data = torch.FloatTensor(np.transpose(valid_data[index, :, :, 4-center:5+center, 4-center:5+center,:], (4,0,1,2,3)))
         test_label =  torch.FloatTensor(self.valdata_label[index, :,:]).unsqueeze(0)
-        return  valid_data,test_label, name 
+        return  valid_data,test_label, name  # 81*512*512  512*512  array
