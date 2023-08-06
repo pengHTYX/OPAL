06fb7f9bf9e38eb634097e9c9e9c0b0d1885cf28
Joey1998hub <50704613+Joey1998hub@users.noreply.github.com>
2023-08-06 13:30:29 +0800

    Update data_util.py

--- data/data_util.py
+++ data/data_util.py
@@ -4,91 +4,6 @@ import torch
 import tifffile as tif
 
 root = './dataset/hci_dataset/'
-stanford_root = './dataset/stanford/'
-hciold_root = './dataset/HCIOLD/'
-
-def generate_hciold_for_train(traindata_all, traindata_label, input_size, ouc):
-    """ initialize image_stack & label """
-    traindata_batch = np.zeros(
-        ( input_size, input_size, 9, 9, ouc),
-        dtype=np.float32)
-    traindata_batch_label = np.zeros((1, input_size, input_size))
-
-    """ inital variable """
-    crop_half1 = 0
-
-    """ Generate image stacks"""
-    sum_diff = 0
-    while (sum_diff < 0.01 * input_size * input_size ):
-        # rand_3color = 0.05 + np.random.rand(3)
-        # rand_3color = rand_3color / np.sum(rand_3color)
-        # R = rand_3color[0]
-        # G = rand_3color[1]
-        # B = rand_3color[2]
-        R, G, B = 0.299, 0.587, 0.114
-
-        aa_arr = np.arange(7)
-        image_id = np.random.choice(aa_arr)
-        ix_rd = 0
-        iy_rd = 0
-
-        kk = np.random.randint(14)
-        scale = 1
-        if (kk < 8):
-            scale = 1
-        elif (kk < 14):
-            scale = 2
-        
-        hh, ww = np.shape(traindata_label[image_id])
-        idx_start = np.random.randint(0, hh - scale * input_size)
-        idy_start = np.random.randint(0, ww - scale * input_size)
- 
-        image_center = (1 / 255) * np.squeeze(
-                R * traindata_all[image_id][idx_start: idx_start + scale * input_size:scale,
-                    idy_start: idy_start + scale * input_size:scale, 4 + ix_rd, 4 + iy_rd, 0].astype('float32') +
-                G * traindata_all[image_id][idx_start: idx_start + scale * input_size:scale,
-                    idy_start: idy_start + scale * input_size:scale, 4 + ix_rd, 4 + iy_rd, 1].astype('float32') +
-                B * traindata_all[image_id][idx_start: idx_start + scale * input_size:scale,
-                    idy_start: idy_start + scale * input_size:scale, 4 + ix_rd, 4 + iy_rd, 2].astype('float32'))
-
-        sum_diff = np.sum(
-                np.abs(image_center - np.squeeze(image_center[int(0.5 * input_size), int(0.5 * input_size)])))
-        
-        if ouc == 1:
-            traindata_batch= np.expand_dims(np.squeeze(
-                R * traindata_all[image_id:image_id + 1, idx_start: idx_start + scale * input_size:scale,
-                    idy_start: idy_start + scale * input_size:scale, :, :, 0].astype(
-                    'float32') +
-                G * traindata_all[image_id:image_id + 1, idx_start: idx_start + scale * input_size:scale,
-                    idy_start: idy_start + scale * input_size:scale, :, :, 1].astype(
-                    'float32') +
-                B * traindata_all[image_id:image_id + 1, idx_start: idx_start + scale * input_size:scale,
-                    idy_start: idy_start + scale * input_size:scale, :, :, 2].astype(
-                    'float32')), -1) 
-        else:
-            traindata_batch =  traindata_all[image_id][idx_start: idx_start + scale * input_size:scale,
-                    idy_start: idy_start + scale * input_size:scale, :, :, :].astype(
-                    'float32')
-               
-
-        '''
-            traindata_batch_label  <-- scale_factor*traindata_label[random_index, scaled_label_size, scaled_label_size] 
-        '''
-        traindata_batch_label[0,:, :] = (1.0 / scale) * traindata_label[image_id][idx_start + scale * crop_half1: idx_start + scale * crop_half1 + scale * input_size:scale,
-                                                                idy_start + scale * crop_half1: idy_start + scale * crop_half1 + scale * input_size:scale]
-
-                                                                
-    traindata_batch = np.float32((1 / 255) * traindata_batch)
-    
-    '''data argument'''
-    # # contrast
-    gray_rand = 0.4 * np.random.rand() + 0.8
-    traindata_batch= pow(traindata_batch, gray_rand)
-    traindata_batch = np.transpose(traindata_batch, (4,0,1,2,3))
-     
-    traindata_all = torch.FloatTensor(traindata_batch)
-    label_all = torch.FloatTensor(traindata_batch_label)
-    return traindata_all, label_all
 
 def generate_hci_for_train(traindata_all, traindata_label, input_size, label_size, batch_size,
                                  Setting02_AngualrViews, boolmask_img4, boolmask_img6, boolmask_img15, use_v, ouc, num_img):
@@ -274,44 +189,6 @@ def load_hci(dir_LFimages):
         image_id=image_id+1
     return traindata_all, traindata_label
 
-def load_hciold(dir_LFimages):    
-    traindata_all = []
-    traindata_label = []
-    for dir_LFimage in dir_LFimages:
-        print(dir_LFimage)
-        try:
-            tmp  = np.float32(imageio.imread(hciold_root+dir_LFimage+'/1.png')) # load LF images(9x9) 
-        except:
-            print(hciold_root+dir_LFimage+'/1.png does not exist...' )
-        h,w,_ = np.shape(tmp)
-     
-        del tmp
-        if h%8 != 0:
-            nn = h//8
-            h = 8*nn
-        if w%8 != 0:
-            nn = w//8
-            w = 8*nn
-        traindata_=np.zeros( (h, w, 9, 9, 3),np.uint8)
-        trainlabel_ = np.zeros((h, w),np.float32)
-        for i in range(81):
-            try:
-                tmp  = np.float32(imageio.imread(hciold_root+dir_LFimage+'/%d.png' % (i+1))) # load LF images(9x9) 
-            except:
-                print(hciold_root+dir_LFimage+'/%d.png does not exist...' % (i+1))
-            traindata_[:,:,i//9,i-9*(i//9),:]=tmp[:h,:w,:]
-            del tmp
-        try:            
-            tmp = np.float32(tif.imread(hciold_root+dir_LFimage+'/disp.tif')) # load LF disparity map
-        except:
-            print(hciold_root+dir_LFimage+'/disp.tif..does not exist' )            
-        trainlabel_ = tmp[:h,:w]
-        del tmp
-        traindata_all.append(traindata_)
-        traindata_label.append(trainlabel_)
-    return traindata_all, traindata_label
-
-
 def read_pfm(fpath, expected_identifier="Pf"):
     # PFM format definition: http://netpbm.sourceforge.net/doc/pfm.html
     
@@ -358,34 +235,3 @@ def read_pfm(fpath, expected_identifier="Pf"):
         except:
             raise Exception('Invalid binary values. Could not create %dx%d array from input.' % (height, width))
         return data
-
-def load_stanford_data(dir_LFimages):
-    traindata_all = []
-    label_all = []
-    for dir_LFimage in dir_LFimages:
-        print(dir_LFimage)
-        try:
-            tmp  = np.float32(imageio.imread(stanford_root+dir_LFimage+'/1.png')) # load LF images(9x9) 
-        except:
-            print(stanford_root+dir_LFimage+'/1.png does not exist...' )
-        h,w,_ = np.shape(tmp)
-     
-        del tmp
-        if h%8 != 0:
-            nn = h//8
-            h = 8*nn
-        if w%8 != 0:
-            nn = w//8
-            w = 8*nn
-        traindata_=np.zeros( (h, w, 9, 9, 3),np.uint8)
-        trainlabel_ = np.zeros((h, w),np.float32)
-        for i in range(81):
-            try:
-                tmp  = np.float32(imageio.imread(stanford_root+dir_LFimage+'/%d.png' % i)) # load LF images(9x9) 
-            except:
-                print(stanford_root+dir_LFimage+'/%d.png does not exist...' % i)
-            traindata_[:,:,i//9,i-9*(i//9),:]=tmp[:h,:w,:]
-            del tmp
-        label_all.append(trainlabel_)
-        traindata_all.append(traindata_)
-    return traindata_all, label_all
